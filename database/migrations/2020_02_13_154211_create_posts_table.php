dbd08a088c1e8cd95e6e232063e3f788307c3e38
Peak Xin <xinyflove@sina.com>
2020-05-15 23:35:14 +0800

    init

---
+++ database/migrations/2020_02_13_154211_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100)->default('');
+            $table->text('content');
+            $table->integer('user_id')->default(0);
+			$table->tinyInteger('status')->default(0);// 文章的状态 0未知/1通过/-1删除
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
